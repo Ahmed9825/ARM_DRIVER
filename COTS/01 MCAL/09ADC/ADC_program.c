68f28f3e7b875c406151b03b70171ac2de4d3e80
Ahmed9825 <132295344+Ahmed9825@users.noreply.github.com>
2023-05-23 17:14:06 +0300

    Add files via upload

--- COTS/01 MCAL/09ADC/ADC_program.c
+++ COTS/01 MCAL/09ADC/ADC_program.c
@@ -1,172 +1,199 @@
-#include	"Bit_Math.h"
-#include 	"Std_Types.h"
+#include "ADC_interface.h"
 
-#include 	"ADC_private.h"
-#include 	"RCC_private.h"
-#include 	"DMA_private.h"
-#include 	"DIO_private.h"
-void ADC_Init (void)
-{
-	/************** STEPS TO FOLLOW *****************
-	1. Enable ADC and GPIO clock
-	2. Set the prescalar in the Clock configuration register (RCC_CFGR)
-	3. Set the Scan Mode and Resolution in the Control Register 1 (CR1)
-	4. Set the Continuous Conversion, EOC, and Data Alignment in Control Reg 2 (CR2)
-	5. Set the Sampling Time for the channels in ADC_SMPRx
-	6. Set the Regular channel sequence length in ADC_SQR1
-	7. Set the Respective GPIO PINs in the Analog Mode
-	************************************************/
+void	ADC_voidInit(void){
 
-//1. Enable ADC and GPIO clock
-	RCC_APB2ENR |= 1<<9;  // enable ADC1 clock
-	RCC_APB2ENR |= (1<<2);  // enable GPIOA clock
-
-//2. Set the prescalar in the Clock configuration register (RCC_CFGR)
-	RCC_CFGR |= (2<<14);  // Prescaler 6, ADC Clock = 72/6 = 12 MHz
-
-//3. Set the Scan Mode and Resolution in the Control Register 1 (CR1)
-	ADC1->CR1 = (1<<8);    // SCAN mode enabled
-	// Resolution is 12 bit in F103
-
-//4. Set the Continuous Conversion, EOC, and Data Alignment in Control Reg 2 (CR2)
-	ADC1->CR2 = (1<<1);     // enable continuous conversion mode
-	// EOC after each conversion by default
-	ADC1->CR2 |= (7<<17);  // External Event selection pointed to SWSTART bit
-	ADC1->CR2 &= ~(1<<11);   // Data Alignment RIGHT
-
-//5. Set the Sampling Time for the channels
-	ADC1->SMPR2 &= ~((7<<3) | (7<<12));  // Sampling time of 1.5 cycles for channel 1 and channel 4
-
-//6. Set the Regular channel sequence length in ADC_SQR1
-	ADC1->SQR1 |= (2<<20);   // SQR1_L =2 for 3 conversions
-
-//7. Set the Respective GPIO PINs in the Analog Mode
-	GPIOA_CRL &= ~(0xf<<4);  // analog mode for PA 1
-	GPIOA_CRL &= ~(0xf<<16); // analog mode for PA 4
-
-
-	/**************************************************************************************************/
-
-
-	// Sampling Freq for Temp Sensor
-	ADC1->SMPR1 |= (7<<18);  // Sampling time (71.5 cycles) of 7 us for channel 16.. It should be <17.1 us
-
+	ADC_ScanConvMode() ;  // SCAN mode enabled
+	ADC_singOrcontainusConvMode();    // enable continuous conversion mode
+	ADC_ExternalTrigConv();  // External Event selection pointed to SWSTART bit
+	ADC_DataAligment();   // Data Alignment RIGHT
+	//Set the Regular channel sequence length in ADC_SQR1
+	ADC_NbrOfConversion();
 	// Set the TSVREFE Bit to wake the sensor
-	ADC1->CR2 |= (1<<23);
+	ADC_tempSenEnable();
+	// Enable or disable  DMA for ADC
+	ADC_DMAenable();
 
-	// Enable DMA for ADC
-	ADC1->CR2 |= (1<<8);
-
-//	// Enable Continuous Request
-//	ADC1->CR2 |= (1<<9);
-
-	// Channel Sequence
-	ADC1->SQR3 |= (1<<0);  // SEQ1 for Channel 1
-	ADC1->SQR3 |= (4<<5);  // SEQ2 for CHannel 4
-	ADC1->SQR3 |= (16<<10);  // SEQ3 for CHannel 16
 }
 
 
-void ADC_Enable (void)
-{
-	/************** STEPS TO FOLLOW *****************
-	1. Enable the ADC by setting ADON bit in CR2
-	2. Wait for ADC to stabilize (approx 10us)
-	************************************************/
-	ADC1->CR2 |= 1<<0;   // ADON =1 enable ADC1
-
+//adc enable
+void	ADC_voidEnable(void){
+SET_BIT(ADC1->CR2,0);  // ADON =1 enable ADC1
 	u32 delay = 10000;
 	while (delay--);
 }
+//ADC_start adc
+void ADC_Start(void){
+	 // Clear Status register
+	CLR_BIT(  ADC1->SR,0);
+	CLR_BIT(  ADC1->SR,1);
+	CLR_BIT(  ADC1->SR,2);
+	CLR_BIT(  ADC1->SR,3);
+	CLR_BIT(  ADC1->SR,4);
 
-void ADC_Start(void)
-{
-	/************** STEPS TO FOLLOW *****************
-	1. Clear the Status register
-	2. Start the Conversion by Setting the SWSTART bit in CR2
-	*************************************************/
-	ADC1->SR = 0;                      // Clear Status register
-	ADC1->CR2 |= (1<<20);              // Conversion on external event enabled
-	ADC1->CR2 |= 1<<22;                // Start conversion
+	SET_BIT(ADC1->CR2 ,20);// Conversion on external event enabled
+	SET_BIT(ADC1->CR2,22);// Start conversion
 }
 
-void DMA_Init (void)
-{
-	/************** STEPS TO FOLLOW *****************
-	1. Enable DMA clock
-	2. Set the DATA Direction
-	3. Enable/Disable the Circular Mode
-	4. Enable/Disable the Memory Increment and Peripheral Increment
-	5. Set the Data Size
-	6. Select the channel for the Stream
-	************************************************/
+void ADC_channelSetup(u8 channel , u8 SEQuance,u8 SamplingTime){
+switch(SEQuance){
+case SEQ1:	ADC1->SQR3 |= (channel<<0); 		  break;
+case SEQ2:	ADC1->SQR3 |= (channel<<5); 		  break;
+case SEQ3:	ADC1->SQR3 |= (channel<<10); 		  break;
+case SEQ4:	ADC1->SQR3 |= (channel<<15); 		  break;
+case SEQ5:	ADC1->SQR3 |= (channel<<20); 		  break;
+case SEQ6:	ADC1->SQR3 |= (channel<<25); 		  break;
+case SEQ7:	ADC1->SQR2 |= (channel<<0); 		  break;
+case SEQ8:	ADC1->SQR2 |= (channel<<5);			  break;
+case SEQ9:	ADC1->SQR2 |= (channel<<10);		  break;
+case SEQ10:	ADC1->SQR2 |= (channel<<15);		  break;
+case SEQ11:	ADC1->SQR2 |= (channel<<20);		  break;
+case SEQ12:	ADC1->SQR2 |= (channel<<25);		  break;
+case SEQ13:	ADC1->SQR1 |= (channel<<0);			  break;
+case SEQ14:	ADC1->SQR1 |= (channel<<5);			  break;
+case SEQ15:	ADC1->SQR1 |= (channel<<10);		  break;
+case SEQ16:	ADC1->SQR1 |= (channel<<15);		  break;
+}
+switch (channel){
+case ADC_channe0: ADC1->SMPR2 |= (SamplingTime<<0);		break;
+case ADC_channe1: ADC1->SMPR2 |= (SamplingTime<<3);		break;
+case ADC_channe2: ADC1->SMPR2 |= (SamplingTime<<6);		break;
+case ADC_channe3: ADC1->SMPR2 |= (SamplingTime<<9);		break;
+case ADC_channe4: ADC1->SMPR2 |= (SamplingTime<<12);		break;
+case ADC_channe5: ADC1->SMPR2 |= (SamplingTime<<15);		break;
+case ADC_channe6: ADC1->SMPR2 |= (SamplingTime<<18);		break;
+case ADC_channe7: ADC1->SMPR2 |= (SamplingTime<<21);		break;
+case ADC_channe8: ADC1->SMPR2 |= (SamplingTime<<24);		break;
+case ADC_channe9: ADC1->SMPR2 |= (SamplingTime<<27);		break;
 
-	// 1. Enable DMA1 Clock
-	RCC_AHBENR |= 1<<0;
+case ADC_channe10: ADC1->SMPR1 |= (SamplingTime<<0);		break;
+case ADC_channe11: ADC1->SMPR1 |= (SamplingTime<<3);		break;
+case ADC_channe12: ADC1->SMPR1 |= (SamplingTime<<6);		break;
+case ADC_channe13: ADC1->SMPR1 |= (SamplingTime<<9);		break;
+case ADC_channe14: ADC1->SMPR1 |= (SamplingTime<<12);		break;
+case ADC_channe15: ADC1->SMPR1 |= (SamplingTime<<15);		break;
+case ADC_channe16: ADC1->SMPR1 |= (SamplingTime<<18);		break;
+case ADC_channe17: ADC1->SMPR1 |= (SamplingTime<<21);		break;
 
-	// 2. Set the Data Direction
-//	DMA1_Channel7->CCR |= (1<<4);   // Read From Memory
-	DMA1->Channel[0].CCR &= ~(1<<4);   // Read From Peripheral
-
-	// 2. Enable the circular mode (CIRC)
-	DMA1->Channel[0].CCR |= 1<<5;
-
-	// 3. Enable the Memory Increment (MINC)
-	DMA1->Channel[0].CCR |= 1<<7;
-
-	// 4. Set the Peripheral data size (PSIZE)
-	DMA1->Channel[0].CCR |= (1<<8);  // 01 : 8 Bit Data
-
-	// 5. Set the Memory data size (MSIZE)
-	DMA1->Channel[0].CCR |= (1<<10);  // 01 : 8 Bit Data
 }
 
-
-void DMA_Config (u32 srcAdd, u32 destAdd, u16 size)
-{
-
-	/************** STEPS TO FOLLOW *****************
-	1. Set the Data Size in the CNDTR Register
-	2. Set the Peripheral Address and the Memory Address
-	3. Enable the DMA Stream
-
-		 Some peripherals don't need a start condition, like UART, So as soon as you enable the DMA, the transfer will begin
-		 While Peripherals like ADC needs the Start condition, so Start the ADC later in the program, to enable the transfer
-	************************************************/
-
-	DMA1->Channel[0].CNDTR = size;   // Set the size of the transfer
-
-	DMA1->Channel[0].CPAR = srcAdd;  // Source address is peripheral address
-
-	DMA1->Channel[0].CMAR = destAdd;  // Destination Address is memory address
-
-	// Enable the DMA Stream
-	DMA1->Channel[0].CCR |= (1<<0);  // EN =1
 }
 
-
-u16 RxData[3];
-float Temperature;
-
-int main(void)
-{
-
-
-
-	ADC_Init ();
-	ADC_Enable ();
-	DMA_Init ();
-
-	DMA_Config ((u32 ) &ADC1->DR, (u32) RxData, 3);
-
-	ADC_Start ();
-
-	while (1)
-	{
-
-		Temperature = ((1.43 - ((float)(3.3*RxData[2]/(float)4095))) / 0.0043) + 25;
-
-		_delay_ms(1000);
+//get flag status
+u8 ADC_getStatusflag(u8 flag){
+	u8 local_val = 0 ;
+	if (flag == AWD){
+		local_val=GET_BIT(ADC1->SR,0);
+	}else if (flag ==EOC ){
+		local_val=GET_BIT(ADC1->SR,1);
+	}else if (flag ==JEOC ){
+		local_val=GET_BIT(ADC1->SR,2);
+	}else if (flag == JSTRT){
+		local_val=	GET_BIT(ADC1->SR,3);
+	}else if (flag==STRT ){
+		local_val=GET_BIT(ADC1->SR,4);
+	}else {
+		//wrong status flag select
 	}
 
+	return local_val ;
+}
+
+
+//clear flag status
+void AD_clearStatusflag(u8 flag){
+	if (flag == AWD){
+			CLR_BIT(ADC1->SR,0);
+		}else if (flag ==EOC ){
+			CLR_BIT(ADC1->SR,1);
+		}else if (flag ==JEOC ){
+			CLR_BIT(ADC1->SR,2);
+		}else if (flag == JSTRT){
+			CLR_BIT(ADC1->SR,3);
+		}else if (flag==STRT ){
+			CLR_BIT(ADC1->SR,4);
+		}else {
+			/*wrong status flag select*/
+		}
+}
+
+void ADC_ScanConvMode(void){
+
+	if(ADC1_ScanConvMode == ADC_ENABLE){
+		SET_BIT(ADC1->CR1,8);
+	}else if(ADC1_ScanConvMode == ADC_DISABLE){
+		CLR_BIT(ADC1->CR1,8);
+	}else{
+		//error in config
+	}
+}
+
+void ADC_singOrcontainusConvMode(void){
+
+	if( ADC1_MODE 	==   CON_MODE ){
+		SET_BIT(ADC1->CR2,1);
+	}else if(ADC1_MODE == SINGLE_MODE){
+		CLR_BIT(ADC1->CR2,1);
+	}else{
+		//error in config
+	}
+}
+
+
+void ADC_ExternalTrigConv(void){
+	if(ADC1_ExternalTrigConv==TIMER1_CC1){
+		ADC1->CR2 |= (0<<17);
+	}else if(ADC1_ExternalTrigConv==TIMER1_CC2){
+		ADC1->CR2 |= (1<<17);
+	}else if(ADC1_ExternalTrigConv==TIMER1_CC3){
+		ADC1->CR2 |= (2<<17);
+	}else if(ADC1_ExternalTrigConv==TIMER2_CC2){
+		ADC1->CR2 |= (3<<17);
+	}else if(ADC1_ExternalTrigConv==TIMER3_TRGo){
+		ADC1->CR2 |= (4<<17);
+	}else if(ADC1_ExternalTrigConv==TIMER4_CC4){
+		ADC1->CR2 |= (5<<17);
+	}else if(ADC1_ExternalTrigConv==Ext1_LINE11){
+		ADC1->CR2 |= (6<<17);
+	}else if(ADC1_ExternalTrigConv==SWSTART){
+		ADC1->CR2 |= (7<<17);
+	}else{
+		//error selecting external trigger
+	}
+
+}
+void ADC_DataAligment(void){
+	if(DATA_MODE == DATA_ALIGN_Right){
+		CLR_BIT(ADC1->CR2,11);
+	}else if (DATA_MODE  == DATA_ALIGN_Left ){
+		SET_BIT(ADC1->CR2,11);
+	}else{
+		//error inalligment config
+	}
+
+}
+void ADC_NbrOfConversion(void){
+	ADC1->SQR1 |= ((ADC1_NbrOfConversion-1)<<20);
+}
+
+void ADC_tempSenEnable(void){
+
+	if(ADC1_tempSen == ADC_ENABLE){
+		SET_BIT(ADC1->CR2,23);
+	}else if(ADC1_tempSen == ADC_DISABLE){
+		CLR_BIT(ADC1->CR2,23);
+	}else{
+		//error in temp config
+	}
+}
+
+
+void ADC_DMAenable (void){
+	if(ADC1_DMA == ADC_ENABLE){
+			SET_BIT(ADC1->CR2,8);
+		}else if(ADC1_DMA == ADC_DISABLE){
+			CLR_BIT(ADC1->CR2,8);
+		}else{
+			//error in DMA config
+		}
 }
